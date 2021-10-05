00d690f6d35e9c374e828700efcaeb6264c242bb
CyrusWarner <cyruswarner27@gmail.com>
2021-10-05 10:43:57 -0500

    Added registration form to register component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
+
 const routes: Routes = [
   {path: 'register', component: RegisterComponent}
 ]
