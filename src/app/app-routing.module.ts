7b19472352eafcbd10a3cdb57fc33f54221beb1d
CyrusWarner <cyruswarner27@gmail.com>
2021-10-05 09:30:29 -0500

    Initial commit.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+const routes: Routes = [
+  {path: 'register', component: RegisterComponent}
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
