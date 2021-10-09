8b290bd7dae6cc9b25f02654dddcecec2fc90549
CyrusWarner <cyruswarner27@gmail.com>
2021-10-09 11:41:47 -0500

    Added reactive form to register component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegisterComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  registerForm: FormGroup
+  firstName:FormControl
+  lastName:FormControl
+  userName:FormControl
+  email:FormControl
+  password:FormControl
+  confirmPassword:FormControl
   constructor() { }
 
   ngOnInit(): void {
+    this.firstName = new FormControl('', Validators.required);
+    this.lastName =  new FormControl('', Validators.required)
+    this.userName = new FormControl('', Validators.required)
+    this.email = new FormControl('', Validators.required)
+    this.password = new FormControl('', Validators.required)
+    this.confirmPassword = new FormControl('', Validators.required)
+
+    this.registerForm = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      userName: this.userName,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword,
+
+    })
+  }
+
+  registerUser(formValues){
+    console.log(formValues);
+
   }
 
 }
