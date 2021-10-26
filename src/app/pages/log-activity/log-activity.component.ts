14eb46115da819b57d29b63123794aab76c90c63
CyrusWarner <cyruswarner27@gmail.com>
2021-10-26 11:23:01 -0500

    Added the log activity component form.

--- src/app/pages/log-activity/log-activity.component.ts
+++ src/app/pages/log-activity/log-activity.component.ts
@@ -3,16 +3,27 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-log-activity',
   templateUrl: './log-activity.component.html',
-  styleUrls: ['./log-activity.component.css']
+  styleUrls: ['./log-activity.component.css'],
 })
 export class LogActivityComponent implements OnInit {
   logActivityForm: FormGroup;
-  constructor() { }
+  activityDate: FormControl;
+  exerciseType: FormControl;
+  activityDistance: FormControl;
+  constructor() {}
 
   ngOnInit(): void {
+    this.activityDate = new FormControl('', Validators.required);
+    this.exerciseType = new FormControl('', Validators.required);
+    this.activityDistance = new FormControl('', Validators.required);
     this.logActivityForm = new FormGroup({
-
-    })
+      activityDate: this.activityDate,
+      exerciseType: this.exerciseType,
+      activityDistance: this.activityDistance
+    });
   }
 
+  saveActivity(formValues) {
+    console.log(formValues);
+  }
 }
